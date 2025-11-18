509a0a085f998fd375242b1b3fe0c7d6093b6986
KonomiKawamoto <konomi.kawamoto@gmail.com>
2025-11-18 20:01:23 +0900

    Add: プレミアム会員制度ページ新規追加　Mod: FB修正

---
+++ js/shopinfo.js
@@ -0,0 +1,25 @@
+window.addEventListener("DOMContentLoaded", () => {
+	let currentIndex = 0;
+	const filterItem  = document.getElementsByClassName('p-shopinfo-filter-list-item');
+	const detailItem  = document.getElementsByClassName('detail-list-item');
+	const flyerSlider = document.getElementsByClassName('p-shopinfo-flyer-slider');
+	const infoItem    = document.getElementsByClassName('info-list-item');
+
+	for (let i = 0; i < filterItem.length; i++) {
+		filterItem[i].addEventListener('click', () => {
+			if(currentIndex != i) {
+				filterItem[currentIndex].classList.remove('active');
+				detailItem[currentIndex].classList.remove('active');
+				flyerSlider[currentIndex].classList.remove('active');
+				infoItem[currentIndex].classList.remove('active');
+			}
+
+			filterItem[i].classList.add('active');
+			detailItem[i].classList.add('active');
+			flyerSlider[i].classList.add('active');
+			infoItem[i].classList.add('active');
+
+			currentIndex = i;
+		});
+	}
+});
